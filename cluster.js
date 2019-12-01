11a0a1a006652cba935a4cde562c194b51555fb4
sharonwe <sharonwe@etoro.com>
2019-12-01 11:02:04 +0200

    add log to cluster

--- cluster.js
+++ cluster.js
@@ -4,7 +4,7 @@ const os = require('os');
 
 if (cluster.isMaster) {
   const cpus = os.cpus().length;
-
+  console.log('test');
   console.log(`Forking for ${cpus} CPUs`);
   for (let i = 0; i<cpus; i++) {
     cluster.fork();
